13cc07c188454cf22f819d44b7f52e38d669e7ac
mrlx <leoixeu@hotmail.fr>
2020-12-06 21:42:01 +0100

    loop blueprint

---
+++ request_loop.py
@@ -0,0 +1,25 @@
+import random 
+
+
+
+rand=random.uniform(0.04, 0.06)
+
+def count():
+	start = 0
+	end = 60
+
+	c=0
+	while(start<end):
+
+		start = start+ rand
+		c=c+1
+		if (c==999):
+			print(start)
+			print(c)
+			return c
+
+	print(c)
+	return c
+
+
+count()

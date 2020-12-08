632a009a35f04215282ded55ea0aad5c51e7d4fd
mrlx <leoixeu@hotmail.fr>
2020-12-08 19:28:46 +0100

    Stress test with locust

--- locust_test.py
+++ locust_test.py
@@ -5,13 +5,15 @@ import json
 
 class SampleTask(TaskSet):
 	@task
+	
 	def post_stress(self):
 		searchstr = {'str':'I love America'}
-		self.client.post("/http://0.0.0.0:5000/text", data=searchstr)
+		self.client.get("/text",data=searchstr)
 
 
 
 class stressTest(HttpUser):
 	tasks = [SampleTask]
-	max_wait = 5000
-	host="http://0.0.0.0:5000/text"
+	min_wait = 60
+	#max_wait = 10000
+	host=("http://0.0.0.0:5000")

efa7394e2c2171fdfc95a234be78d5f77581da6a
mrlx <leoixeu@hotmail.fr>
2020-12-13 21:03:32 +0100

    Final version add content test

--- test_app.py
+++ test_app.py
@@ -1,39 +1,44 @@
 import unittest
 import requests
 import os
-import time
 
 
 class Testing(unittest.TestCase):
+
+	
 
 #Beginning of the test
 
 	def setUP(self):
 	
 		os.environ['NO_PROXY'] = '0.0.0.0'
+		os.environ['NO_PROXY'] = '0.0.0.0/text'
 
+		
+	def test_access_web_api(self):
+		resp = requests.get('http://localhost:5000/text')
+		self.assertEqual(resp.status_code,200)
 
 	def test_request_success(self):
 	
 		searchstr = {'nm' : 'I love America'}
 		responce = requests.post('http://0.0.0.0:5000/text',data=searchstr)
 		self.assertEqual(responce.status_code,200) 
+		self.assertRegex(str(responce.content),'[+@+#]')
 		
-	#def test_return_content(self):
+	def test_no_similarity(self):
+		searchstr = {'nm' : 'anticonstitutionnellement'}
+		responce = requests.post('http://0.0.0.0:5000/text',data=searchstr)
+		self.assertEqual(responce.content,b'No similar tweet found')
 	
-		#self.asserIsNotNone(responce.content)
-		#self.assertRegex(responce.content, '[A-Z]')
-		
-	#def test_no(self):
-	#	self.asserEqual(responce.content,'no similar tweet found for now')
-	
-	
+def tearDown(self):
+			pass
+					
 	
 	
 if __name__ == '__main__':
 	unittest.main()
 #End of the test
 
-	def tearDown(self):
-		pass
+	
 		

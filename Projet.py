a0985c99f90fe3a330177ffad20c7c1dfbfdc187
AbderrahmaneAdda <abderrahmane.adda15gmail.com>
2020-12-14 21:29:13 +0100

    update

--- Projet.py
+++ Projet.py
@@ -4,7 +4,7 @@ import nltk
 import numpy as np
 from nltk import word_tokenize
 from nltk.corpus import stopwords
-from unidecode import unidecode
+#from unidecode import unidecode
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from gensim.test.utils import common_texts
@@ -14,16 +14,28 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 from flask import jsonify
 from prometheus_client import start_http_server
+from prometheus_client import Counter
+from prometheus_client import Histogram
+import time
+nltk.download('stopwords')
+nltk.download('punkt')
 
 
+REQUESTS = Counter('flask_app_calls_total', 'How many time my app was called')
+EXCEPTIONS = Counter('flask_app_exceptions_total', 'How many exceptions my app was raised')
+LATENCY = Histogram('flask_app_latency_seconds', 'Time needed for a request')
 
 def pre_process(corpus):
     corpus = corpus.lower()
     stopset = stopwords.words('english') + list(string.punctuation)
     corpus = " ".join([i for i in word_tokenize(corpus) if i not in stopset])
-    corpus = unidecode(corpus)
+    #corpus = unidecode(corpus)
+    corpus = str(corpus)
     return corpus
 
+def index_in_list(a_list, index):
+    return (index < len(a_list))
+    
 model = Word2Vec(sentences=common_texts, window=5, min_count=1, workers=4)
 word_emb_model = model
 
@@ -32,60 +44,21 @@ def get_cosine_similarity(feature_vec_1, feature_vec_2):
 
 df = pd.read_csv('tweets.csv')
 df.drop_duplicates(subset ="text", keep = False, inplace = True)
-#df = df.head(1000)
-l = []
-
-#corpus = ['America is terrible right now']
-
-#for c in range(len(corpus)):
- #   corpus[c] = pre_process(corpus[c])
-
-
-#for i in df['Unnamed: 0']:
- #   l1=[]
-  #  df1 = df.loc[lambda df: df['Unnamed: 0'] == i]
-   # a = str(df1['text'])
-   # l1.append(a)
-   # l1[0] = pre_process(l1[0])
-   # tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2))
-   # tfidf_vectorizer.fit(l1)
-   # feature_vectors = tfidf_vectorizer.transform(l1)
-   # feature_vectors1 = tfidf_vectorizer.transform(corpus)
-   # l.append(get_cosine_similarity(feature_vectors, feature_vectors1))
-
-
-#l2=[]
-#for i in range (0,len(l)):
-#    if l[i] !=0:
-#        l2.append(i)
-
-#d = {}
-#for i in l2:
-#    d[i] = l[i]
-
-#d1 = {k: v for k, v in sorted(d.items(), key=lambda item: item[1])}
-#l5=[]
-#for i in range(1,20):
- #   df3 = df.loc[lambda df: df['Unnamed: 0'] == list(d1)[-i]]
-   # a = str(df3['text'])
-    #l5.append(a)
-    #l5.append('<br/>')
-    #joined_string = "".join(l5)
-
-
-#str1 = '\n'.join(l5)
-
+#df = df.head(5000)
+#l = []
 
 
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     return render_template("test.html")
 
 @app.route("/text", methods=["POST","GET"])
 def text():
+    REQUESTS.inc()
+    start = time.time()
     if request.method == "POST":
+        l = []
         corpus = []
         corpus.append(request.form["nm"])
         for c in range(len(corpus)):
@@ -114,27 +87,34 @@ def text():
         for i in l2:
             d[i] = l[i]
 
-        d1 = {k: v for k, v in sorted(d.items(), key=lambda item: item[1])}
+        d1 = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
         l5=[]
         s=0
         for i in range(1,20):
-            df3 = df.loc[lambda df: df['Unnamed: 0'] == list(d1)[i]]
-            s = list(df3['text'])
-            if not s:
-                l5.append("no similar tweet found for now")
-                l5.append('<br/>')
-            else:
-                l5.append(s[0])
-                l5.append('<br/>')
-            
-            
-                
-            #a = list(df3['text'])
-            #a = str(df3['text'])
+        	if index_in_list(list(d1), i):
+            		df3 = df.loc[lambda df: df['Unnamed: 0'] == list(d1)[i]]
+            		s = list(df3['text'])
+            		if not s:
+             			EXCEPTIONS.inc()
+             			l5.append("no similar tweet found for now")
+             			l5.append('<br/>')
+            		else:
+             			l5.append(s[0])
+             			l5.append('<br/>')
+            		
+        	else:
+		        EXCEPTIONS.inc()             		
+		        l5.append("no similar tweet found for now")
+		        l5.append('<br/>')	
+	     
+        
+               
         joined_string = "".join(l5)
-          
+        l5=[] 
+
+        LATENCY.observe(time.time() - start)          
         return joined_string
-          
+                  
           
 
     else:
@@ -147,7 +127,7 @@ def text():
 
 if __name__ == '__main__':
     start_http_server(8081)
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0')
 
 
     
